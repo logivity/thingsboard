bdf6242ff993371c2f935471f16674a79c78b4ff
logivity <mechengg.kamal@gmail.com>
2017-11-15 16:47:55 -0500

    Chnaged logitrix name and icons

--- ui/webpack.config.dev.js
+++ ui/webpack.config.dev.js
@@ -51,7 +51,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
-            title: 'ThingsBoard',
+            title: 'Logitrix',
             inject: 'body',
         }),
         new webpack.optimize.OccurrenceOrderPlugin(),
